8f932a8301ffa197610e1ca6392171d260b1d21e
stiven <deibysp39@gmai.com>
2020-05-04 23:07:20 -0500

    Implemetacion base de datos

---
+++ app/src/main/java/com/example/proyecto/baseDatos/dbUsuarios.java
@@ -0,0 +1,54 @@
+package com.example.proyecto.baseDatos;
+
+import java.io.Serializable;
+
+public class dbUsuarios implements Serializable {
+
+    private int id;
+    private String nameUsuario;
+    private String correo;
+    private String password;
+
+    public dbUsuarios(){
+
+    }
+
+    public dbUsuarios(int id, String nameUsuario, String correo, String password) {
+        this.id = id;
+        this.nameUsuario = nameUsuario;
+        this.correo = correo;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameUsuario() {
+        return nameUsuario;
+    }
+
+    public void setNameUsuario(String nameUsuario) {
+        this.nameUsuario = nameUsuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
