99f9982bf267fa8915f7b42c60f4df9867f25c0c
stiven <deibysp39@gmai.com>
2020-06-16 01:29:47 -0500

    repare errores

---
+++ app/src/main/java/com/example/proyecto/vistaEventos/juanBosco.java
@@ -0,0 +1,13 @@
+package com.example.proyecto.vistaEventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class juanBosco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juan_bosco);
+    }
+}
