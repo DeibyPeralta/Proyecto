87fbff61634351a5e485f697d234fadf8143183f
stiven <deibysp39@gmai.com>
2020-06-17 02:10:30 -0500

    hotel y funcionalidades

---
+++ app/src/main/java/com/example/proyecto/hoteles/tativan.java
@@ -0,0 +1,14 @@
+package com.example.proyecto.hoteles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tativan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tativan);
+    }
+}
