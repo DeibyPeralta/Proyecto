e998238b32b1e12c5666f25d6fb012b48c80040c
stiven <deibysp39@gmai.com>
2020-06-17 01:51:29 -0500

    hoteles.. va 1 apenas

---
+++ app/src/main/java/com/example/proyecto/hoteles/Sicarare.java
@@ -0,0 +1,14 @@
+package com.example.proyecto.hoteles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sicarare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sicarare);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto/hoteles/mayalesPlaza.java
@@ -0,0 +1,13 @@
+package com.example.proyecto.hoteles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mayalesPlaza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mayales_plaza);
+    }
+}
