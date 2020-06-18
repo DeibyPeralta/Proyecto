e9f7616df21184ad19c42e891b3456b386583450
stiven <deibysp39@gmai.com>
2020-06-18 07:46:52 -0500

    recreacion (inicio)

---
+++ app/src/main/java/com/example/proyecto/recreacion/caminata.java
@@ -0,0 +1,14 @@
+package com.example.proyecto.recreacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class caminata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caminata);
+    }
+}
