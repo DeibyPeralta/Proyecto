b3acdbf929b21ba40fe6b2ed517ed90298a4d529
stiven <deibysp39@gmai.com>
2020-06-18 02:12:07 -0500

    Modulo nocturno terminado

---
+++ app/src/main/java/com/example/proyecto/nocturnos/palenqueBar.java
@@ -0,0 +1,14 @@
+package com.example.proyecto.nocturnos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class palenqueBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palenque_bar);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto/nocturnos/patioBar.java
@@ -0,0 +1,14 @@
+package com.example.proyecto.nocturnos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class patioBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patio_bar);
+    }
+}
